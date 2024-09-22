62f4302dce30f1635dee1925a17ee55274a2e53f
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-09-22 15:52:25 -0500

    Se crea el proyecto del frontend para ingenieria de software II.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', component: InicioComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'registro', component: RegistroComponent },
+{ path: "**", pathMatch: "full", redirectTo: "" }
+];
